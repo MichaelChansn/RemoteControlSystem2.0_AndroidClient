71c586c11e7b02a858ac306ceac4e1ed30b8c637
MichaelChansn <13672025181@163.com>
2016-01-08 14:37:42 +0800

    code asyn

--- src/com/ks/streamline/ReceiveThread.java
+++ src/com/ks/streamline/ReceiveThread.java
@@ -29,4 +29,24 @@ public class ReceiveThread extends Thread {
 		}
 		
 	}
+	
+	/**数据转换过程中一定要注意符号位扩展问题，自动转型问题,因为&符号两边默认都是int型*/
+	private int conventSmall2Big(int small)
+	{
+		byte[] bits=new byte[4];
+		bits[0]=(byte) (small&0x000000ff);
+		bits[1]=(byte)((small>>>8)&0x000000ff);
+		bits[2]=(byte)((small>>>16)&0x000000ff);
+		bits[3]=(byte)((small>>>24)&0x000000ff);
+		int ret=(bits[0]&0x000000ff)<<24 | (bits[1]&0x000000ff)<<16 | (bits[2]&0x000000ff)<<8 | (bits[3]&0x000000ff);
+		return ret;
+	}
+	private short conventSmall2Big(short small)
+	{
+		byte[] bits=new byte[2];
+		bits[0]=(byte) (small&0x000000ff);
+		bits[1]=(byte)((small>>8)&0x000000ff);
+		short ret=(short)((bits[0]&0x000000ff)<<8 | (bits[1]&0x000000ff));
+		return ret;
+	}
 }
